f7a548f1fe29815de1e26185e67cd2afb8ad2249
pquochuy <quochuy.pham@ynov.com>
2025-10-09 10:29:01 +0200

    9102025

--- main.go
+++ main.go
@@ -10,7 +10,14 @@ func main() {
 	//home routeur
 	http.HandleFunc("/", home)
 	// play
-	http.HandleFunc("/start", start)
+	//http.HandleFunc("/start", start)
+	// Mode de jeux page select 1vs1 ou 1vsrobot
+	http.HandleFunc("/mode", mode)
+	// players
+	http.HandleFunc("/players", players)
+	// optionHandler
+	http.HandleFunc("/options", options)
+
 	http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("static"))))
 	http.ListenAndServe(":8080", nil)
 }
@@ -23,14 +30,42 @@ func home(w http.ResponseWriter, r *http.Request) {
 	template.Execute(w, nil)
 }
 
-func start(w http.ResponseWriter, r *http.Request) {
-	/*if r.Method != http.MethodPost{
-		// 		http.Redirect(w, r, "/", http.StatusSeeOther)
-		// 		return
-	// 	}*/
-	template, erreur := template.ParseFiles("templates/start.html")
+/*func start(w http.ResponseWriter, r *http.Request) {
+/*if r.Method != http.MethodPost{
+	// 		http.Redirect(w, r, "/", http.StatusSeeOther)
+	// 		return
+// 	}*/
+/*	template, erreur := template.ParseFiles("templates/start.html")
 	if erreur != nil {
 		log.Fatal(erreur)
 	}
 	template.Execute(w, nil)
+}*/
+
+func mode(w http.ResponseWriter, r *http.Request) {
+	template, erreur := template.ParseFiles("template/mode.html")
+	if erreur != nil {
+		log.Fatal(erreur)
+	}
+	template.Execute(w, nil)
+}
+func players(w http.ResponseWriter, r *http.Request) {
+	template, erreur := template.ParseFiles("tempates/players.html")
+	if erreur != nil {
+		log.Fatal(erreur)
+	}
+	template.Execute(w, nil)
+}
+func options(w http.ResponseWriter, r *http.Request) {
+	template, erreur := template.ParseFiles("tempates/options.html")
+	if erreur != nil {
+		log.Fatal(erreur)
+	}
+	template.Execute(w, nil)
+}
+
+type Game struct {
+	Board         [6][7]int //
+	CurrentPlayer int
+	GameActive    bool
 }
